ccfb51ba3b481ea99c14bd69a653436774a7ca60
Adam Hall <adamhalldesigns@gmail.com>
2019-10-06 20:01:03 +0100

    WIP: spinner hanging

--- src/init.d/dependencies.js
+++ src/init.d/dependencies.js
@@ -1,60 +1,62 @@
 import chalk from 'chalk';
 import execa from 'execa';
+import ora from 'ora';
 
 export default dependencies;
 
+const devDeps = {
+  common: [
+    "eslint@5.16.0",
+    "eslint-config-prettier",
+    "eslint-plugin-prettier",
+    "prettier",
+  ],
+  node: {
+    common: [
+      "nodemon",
+      "jest",
+      "eslint-config-airbnb-base",
+    ],
+    javascript: [],
+    typescript: [
+      "@types/jest",
+      "@typescript-eslint/eslint-plugin",
+      "@typescript-eslint/parser",
+      "concurrently",
+      "eslint-import-resolver-alias",
+      "ts-jest",
+    ]
+  },
+  react: {
+    common: ["eslint-config-airbnb"],
+    javascript: [],
+    typescript: [
+      '@typescript-eslint/parser',
+      '@typescript-eslint/eslint-plugin',
+    ],
+  }
+}
+
+const deps = {
+  common: [],
+  node: {
+    common: [],
+    javascript: [],
+    typescript: [
+      "typescript",
+      "@types/node",
+      "module-alias",
+    ],
+  },
+  react: {
+    common: [],
+    javascript: [],
+    typescript: [],
+  }
+}
+
 async function dependencies({ project, lang, title, logger }) {
   logger.info('setting up dependencies')
-  const devDeps = {
-    common: [
-      "eslint@5.16.0",
-      "eslint-config-prettier",
-      "eslint-plugin-prettier",
-      "prettier",
-    ],
-    node: {
-      common: [
-        "nodemon",
-        "jest",
-        "eslint-config-airbnb-base",
-      ],
-      javascript: [],
-      typescript: [
-        "@types/jest",
-        "@typescript-eslint/eslint-plugin",
-        "@typescript-eslint/parser",
-        "concurrently",
-        "eslint-import-resolver-alias",
-        "ts-jest",
-      ]
-    },
-    react: {
-      common: ["eslint-config-airbnb"],
-      javascript: [],
-      typescript: [
-        '@typescript-eslint/parser',
-        '@typescript-eslint/eslint-plugin',
-      ],
-    }
-  }
-
-  const deps = {
-    common: [],
-    node: {
-      common: [],
-      javascript: [],
-      typescript: [
-        "typescript",
-        "@types/node",
-        "module-alias",
-      ],
-    },
-    react: {
-      common: [],
-      javascript: [],
-      typescript: [],
-    }
-  }
 
   const depsL = [
     ...deps.common,
@@ -72,11 +74,8 @@ async function dependencies({ project, lang, title, logger }) {
     'info', airbnbPack, 'peerDependencies', '--json'
   ]);
 
-  if (!stdout) {
-    return Promise.reject(new Error('Failed to fetch dependencies for airbnb'));
-  }
-
   const airbnbDeps = Object.keys(JSON.parse(stdout)) // .filter(dep => dep !== 'eslint');
+
   logger.info('airbnb dependencies:')
   logger.table(airbnbDeps)
 
@@ -90,25 +89,25 @@ async function dependencies({ project, lang, title, logger }) {
   logger.info('all dev dependencies:')
   logger.table(devDepsL)
 
-  if (depsL.length > 0) {
-    console.log(chalk.blue('Installing Dependencies...'));
-    console.table(depsL);
+  console.log('\nInstalling Dependencies...')
 
-    const depsResult = await execa('yarn', ['add', ...depsL], { cwd: title })
+  await asyncForEach(depsL, async dep => {
+    const spinner = ora(dep).start();
+    await execa('yarn', ['add', dep], { cwd: title })
+    spinner.succeed();
+    spinner.stop();
+  })
 
-    if (depsResult.failed) {
-      return Promise.reject(new Error('failed to install Dependencies'));
-    }
-  }
+  await asyncForEach(devDepsL, async dep => {
+    const spinner = ora(dep).start();
+    await execa('yarn', ['add', '-D', dep], { cwd: title })
+    spinner.succeed();
+    spinner.stop();
+  })
+}
 
-  if (devDepsL.length > 0) {
-    console.log(chalk.blue('Installing Dev Dependencies...'));
-    console.table(devDepsL);
-
-    const devDepsResult = await execa('yarn', ['add', '-D', ...airbnbDeps, ...devDepsL], { cwd: title })
-
-    if (devDepsResult.failed) {
-      return Promise.reject(new Error('failed to install devDependencies'));
-    }
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
   }
 }
