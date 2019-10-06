e8cbb70fe4755f4308e5736bcbcdfa2924f3c533
Adam Hall <adamhalldesigns@gmail.com>
2019-10-06 12:05:28 +0100

    Add node env validation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import arg from 'arg';
 import {prompt} from './prompt';
 import {init} from './init';
+import checkNodenv from './validateNode';
 
 export async function cli(args) {
   try {
+    checkNodenv();
     const options = await prompt();
     init(options)
   } catch (e) {
---
+++ src/validateNode.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import semver from 'semver';
+import { engines } from '../package.json';
+
+export default checkNodenv;
+
+function checkNodenv() { 
+  const nodeVersion = engines.node;
+
+  if (!semver.satisfies(process.version, nodeVersion)) {
+      console.log(chalk.red('NodeJS Version Check Failed'));
+      console.log(`Required node version ${chalk.yellow(nodeVersion)}`)
+      console.log(`NOT SATISFIED with current version ${ chalk.red(process.version) }.`);
+      console.log(`
+This is the version of node executing js-gen,
+not the version of node that will be used by your generated app.
+
+You may need to change your ${chalk.cyan('global node version')} and rerun
+npm install -g @ahdesigns/js-gen`)
+    process.exit(1);
+  }
+}
