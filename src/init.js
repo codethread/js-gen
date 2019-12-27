85b70cd9050de5560fb3adfef9a51164cb3d3534
Adam Hall <adamhalldesigns@gmail.com>
2019-12-27 11:00:57 +0000

    remove common folder

--- src/init.js
+++ src/init.js
@@ -34,7 +34,6 @@ export async function init(options_raw) {
 
     options.logger.info('copying files...');
     await copyFiles(getTemplates('common'));
-    project === 'react' && await copyFiles(getTemplates(`${project}/common`));
     await copyFiles(getTemplates(`${project}/${lang}`));
 
     await gitInit(options);
